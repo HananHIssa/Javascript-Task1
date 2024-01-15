3b11bca9b8749e9e65689d4b6266d0b2db0672fa
HananHIssa <hananissa2018@gmail.com>
2024-01-15 20:53:21 +0200

    js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-document .querySelector("div").classList.add("demo");
-document .querySelector("h1").classList .add("vip");
-document .querySelector("h1").classList .remove("vip");
+function convertCurrency(event) {
+    event.preventDefault(); 
+    const amount = parseFloat(document.getElementById('amount').value);
+    const currency = document.getElementById('exchange').value;
+    const exchangeRates = {
+      usd: 0.30,   
+      dinar: 0.15, 
+      nis: 1      
+    };
+    
+    const result = amount * exchangeRates[currency];
+    document.querySelector('.result').textContent = `Equivalent value: ${result.toFixed(2)} ${currency.toUpperCase()}`;
+  }
+
+  
